d8ad44c4c82be912a06bce5a346417a367b93f9a
中内 純 <nakauchi.k@g-hits.co.jp>
2019-02-08 18:07:39 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,80 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetLineWidth(2);
+	ofNoFill();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	int span = 72;
+	int radius = 30;
+	int len = radius * 2;
+	for (int x = span * 0.5; x < ofGetWidth(); x += span) {
+
+		for (int y = span * 0.5; y < ofGetHeight(); y += span) {
+
+			ofPushMatrix();
+			ofTranslate(x, y);
+
+			float noise_value = ofNoise(ofRandom(1000), ofGetFrameNum() * 0.01);
+
+			ofBeginShape();
+			for (int deg = 0; deg < 360; deg += 1) {
+
+				glm::vec2 circle_point = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
+				glm::vec2 rect_point = this->make_point(len, ofMap(deg, 0, 360, 87.5, 187.5));
+
+				glm::vec2 difference = circle_point - rect_point;
+				glm::vec2 between_point = rect_point + difference * noise_value;
+
+				ofVertex(between_point);
+			}
+			ofEndShape(true);
+			ofPopMatrix();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+glm::vec2 ofApp::make_point(int len, int param) {
+
+	param = param % 100;
+	if (param < 25) {
+
+		return glm::vec2(ofMap(param, 0, 25, -len * 0.5, len * 0.5), -len * 0.5);
+	}
+	else if (param < 50) {
+
+		return glm::vec2(len * 0.5, ofMap(param, 25, 50, -len * 0.5, len * 0.5));
+	}
+	else if (param < 75) {
+
+		return glm::vec2(ofMap(param, 50, 75, len * 0.5, -len * 0.5), len * 0.5);
+	}
+	else {
+
+		return glm::vec2(-len * 0.5, ofMap(param, 75, 100, len * 0.5, -len * 0.5));
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
